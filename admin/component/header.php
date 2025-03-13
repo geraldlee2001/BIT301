80d713d62a58202f7cd2c791065fd2ee2119e6ac
geraldlee2001 <geraldlee168168@gmail.com>
2025-03-13 10:20:36 +0800

    fix: incorrect header path

--- admin/component/header.php
+++ admin/component/header.php
@@ -7,7 +7,7 @@ if (!$decoded->userId && ($decoded->role !== 'ADMIN' || $decoded->role !== 'MERC
 
 echo '    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
 <!-- Navbar Brand-->
-<a class="navbar-brand ps-3" href="index.php">Start Bootstrap</a>
+<a class="navbar-brand ps-3" href="' . ($decoded->role === 'ADMIN' ? "index.php" : 'products.php') . '">Start Bootstrap</a>
 <!-- Sidebar Toggle-->
 <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
 <!-- Navbar Search-->
